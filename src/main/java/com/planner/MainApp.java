25c3a39b7d460f265be1f9f182ab63458503f8f3
AlexK <alkremn@gmail.com>
2019-07-10 15:24:19 -0600

    Init commit

---
+++ src/main/java/com/planner/MainApp.java
@@ -0,0 +1,20 @@
+package main.java.com.planner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
